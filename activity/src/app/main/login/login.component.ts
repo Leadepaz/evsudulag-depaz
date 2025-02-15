2c83a81aaa70f946c3f48920c7b2065eee05f57b
lea <depazlea059@gmail.com>
2025-02-15 17:25:46 +0800

    Login Page

--- activity/src/app/main/login/login.component.ts
+++ activity/src/app/main/login/login.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: false,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  login: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.login = this.fb.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  get username(){
+    return this.login.get('username');
+  }
+  get password(){
+    return this.login.get('password');
+  }
+
+  onLogin() {
+    if (this.login.valid) {
+      console.log('Login Successful:', this.login.value);
+    }
+  }
 }
--- activity/src/app/main/main-users.module.ts
+++ activity/src/app/main/main-users.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MainRoutesModule } from './main-routes.module';
+import { LoginComponent } from './login/login.component';
+import { MainMaterialModule } from './main-material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ LoginComponent ],
   imports: [
-    CommonModule
+    CommonModule,
+    MainRoutesModule,
+    MainMaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class MainUsersModule { }
